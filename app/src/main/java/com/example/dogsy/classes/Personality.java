cddae5676523d0891bee453f59a0ee23c0fe0a83
Tobias Maier <85940935+maiertbi@users.noreply.github.com>
2022-11-01 01:10:15 +0000

    implemented basic OOP (#7)

---
+++ app/src/main/java/com/example/dogsy/classes/Personality.java
@@ -0,0 +1,30 @@
+package com.example.dogsy.classes;
+
+public class Personality {
+    private int id;
+    private String name;
+
+    public Personality() {
+    }
+
+    public Personality(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
