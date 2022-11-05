b232ac0cd6ad8996a08a55bd8f99077249848f84
Fabian <stffscmi@stud.h-da.de>
2022-11-05 14:11:20 +0000

    made mail and password register screen functional and added input check

---
+++ app/src/main/java/com/example/dogsy/RegisterPassword.java
@@ -0,0 +1,49 @@
+package com.example.dogsy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterPassword extends AppCompatActivity {
+    String userMail;
+    String userPassword1;
+    String userPassword2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_password);
+        Intent mailIntent = getIntent();
+        userMail = mailIntent.getStringExtra("mail");
+        ImageButton ib = (ImageButton) findViewById(R.id.ib_reguserpassword);
+        ib.setOnClickListener(new View.OnClickListener() {
+            EditText et1 = (EditText) findViewById(R.id.et_reguserpassword1);
+            EditText et2 = (EditText) findViewById(R.id.et_reguserpassword2);
+            TextView tv1 = (TextView) findViewById(R.id.tv_wrongPassword);
+
+            @Override
+            public void onClick(View view) {
+                userPassword1 = et1.getText().toString();
+                userPassword2 = et2.getText().toString();
+                if(!userPassword1.equals(userPassword2)){
+                    tv1.setText("Passwords don't match");
+                }
+                else if(userPassword1.isEmpty() || userPassword2.isEmpty()){
+                    tv1.setText("Please enter a password");
+                }
+                else{
+                    Intent registerUser = new Intent(RegisterPassword.this, RegisterUser.class);
+                    registerUser.putExtra("mail", userMail);
+                    registerUser.putExtra("password", userPassword1);
+                    startActivity(registerUser);
+                    finish();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dogsy/RegisterUser.java
@@ -0,0 +1,13 @@
+package com.example.dogsy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterUser extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_user);
+    }
+}
