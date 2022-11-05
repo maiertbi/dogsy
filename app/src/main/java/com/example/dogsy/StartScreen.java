83b7c439da76bffc89fa7e338abcbc89f5f97241
zladb <zla_db@naver.com>
2022-11-05 18:37:21 +0000

    start screen
    connecting to Login, RegisterMail class by clicking button

---
+++ app/src/main/java/com/example/dogsy/StartScreen.java
@@ -0,0 +1,37 @@
+package com.example.dogsy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+    Button log_in;
+    Button sign_up;
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+
+
+        // log in button
+        log_in = findViewById(R.id.button_login);
+
+        // if click the log in button, move to login_activity.
+        sign_up.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+        });
+
+        // sign up button
+        sign_up = findViewById(R.id.button_signup);
+
+        // if click the sign_up button, move to sign_up page.
+        sign_up.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterMail.class);
+            startActivity(intent);
+        });
+    }
+}
