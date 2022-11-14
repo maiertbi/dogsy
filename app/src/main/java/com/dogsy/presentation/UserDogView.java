4cd550fc5048bed69d5f2ab5ccabf5e7ad83bb11
Tobias Maier <2010653293@stud.fh-kufstein.ac.at>
2022-11-14 01:04:27 +0000

    fixed further merging issues

--- app/src/main/java/com/dogsy/presentation/UserDogView.java
+++ app/src/main/java/com/dogsy/presentation/UserDogView.java
@@ -1,4 +1,4 @@
-package com.example.dogsy;
+package com.dogsy.presentation;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.dogsy.R;
 
 /**
  * A simple {@link Fragment} subclass.
