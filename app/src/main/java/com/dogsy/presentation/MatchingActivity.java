040216bbf851fb8326d99283ebb6c551a359e388
Tobias Maier <2010653293@stud.fh-kufstein.ac.at>
2022-11-15 13:46:37 +0000

    tested if fragments work in different activities

--- app/src/main/java/com/dogsy/presentation/MatchingActivity.java
+++ app/src/main/java/com/dogsy/presentation/MatchingActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.dogsy.presentation.fragments.UserDogView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import com.dogsy.R;
@@ -17,13 +18,15 @@ public class MatchingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching);
-
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.matching);
+
+        UserDogView fragment = UserDogView.newInstance(-1);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout_matching, fragment).commit();
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId())
