fd05aff40ae9447076fde0a1e0f216978a55d21b
Tobias Maier <2010653293@stud.fh-kufstein.ac.at>
2022-11-15 14:31:49 +0000

    tested if fragments work in different activities

---
+++ app/src/main/java/com/dogsy/presentation/editProfileActivity.java
@@ -0,0 +1,54 @@
+package com.dogsy.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.dogsy.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class editProfileActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.profile:
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    overridePendingTransition(0, 0);
+                    finish();
+                    return true;
+
+
+                case R.id.matching:
+                    startActivity(new Intent(getApplicationContext(), MatchingActivity.class));
+                    overridePendingTransition(0, 0);
+                    finish();
+                    return true;
+
+                case R.id.chatting:
+                    startActivity(new Intent(getApplicationContext(), ChatActivity.class));
+                    overridePendingTransition(0, 0);
+                    finish();
+                    return true;
+            }
+            return false;
+        });
+    }
+}
