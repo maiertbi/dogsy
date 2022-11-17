3c771f8400b150eccb5e1da9cdfb1f889dbceb1e
zladb <zla_db@naver.com>
2022-11-17 12:31:28 +0000

    implemeting chat-list
    in ChatActivity by using RecyclerView

--- app/src/main/java/com/dogsy/presentation/ChatActivity.java
+++ app/src/main/java/com/dogsy/presentation/ChatActivity.java
@@ -7,10 +7,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.dogsy.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.dogsy.ChatItem;
+import com.dogsy.RecyclerViewAdapter;
+
+import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+
+    RecyclerView mRecyclerView = null;
+    RecyclerViewAdapter mAdapter = null;
+    ArrayList<ChatItem> mList;
+
+    private Drawable mImageDrawable;
+    private String mMainText;
+    private String mSubText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,7 @@ public class ChatActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+        // bottom navigation
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.chatting);
 
@@ -44,5 +65,33 @@ public class ChatActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        // recycler view
+        mRecyclerView = findViewById(R.id.chatting_list_view);
+        mList = new ArrayList<>();
+
+        mAdapter = new RecyclerViewAdapter(mList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        mImageDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.person_plus, null);
+
+        addItem(mImageDrawable, "Fabian",   "hey baby :)");
+        addItem(mImageDrawable, "Yujin",  "Good morning!");
+        addItem(mImageDrawable, "Tobi",  "genius");
+        addItem(mImageDrawable, "David",  "I have a non-binary dog");
+        addItem(mImageDrawable, "Alex",  "Still no answer from her");
+
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void addItem(Drawable icon, String mainText, String subText) {
+        ChatItem item = new ChatItem();
+
+        item.setIcon(icon);
+        item.setTitle(mainText);
+        item.setDesc(subText);
+
+        mList.add(item);
     }
 }
