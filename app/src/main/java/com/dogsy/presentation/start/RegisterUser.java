ba514be96a04722c7ea55b6fb4dfcc01a7053fb1
Tobias Maier <2010653293@stud.fh-kufstein.ac.at>
2022-11-22 13:16:52 +0000

    refactored and made TODO's for RegisterUser

--- app/src/main/java/com/dogsy/presentation/start/RegisterUser.java
+++ app/src/main/java/com/dogsy/presentation/start/RegisterUser.java
@@ -44,17 +44,17 @@ public class RegisterUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_user);
 
-        // TODO: Remove, just for testing.
-        UserService.instance.signOut();
-
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
-        //get data from registerPassword class
-        Intent passwordIntent = getIntent();
-        userMail = passwordIntent.getStringExtra("mail");
-        userPassword = passwordIntent.getStringExtra("password");
+        // TODO: DB - Remove next line. This is just for testing.
+        UserService.instance.signOut();
+
+        // get data from previous Intent
+        Bundle bundle = getIntent().getExtras();
+        userMail = bundle.getString("mail");
+        userPassword = bundle.getString("password");
 
         EditText et_name = findViewById(R.id.et_fn);
         EditText et_dd = findViewById(R.id.et_dd);
@@ -69,7 +69,7 @@ public class RegisterUser extends AppCompatActivity {
         EditText et_park = findViewById(R.id.et_userphone);
 
         //set spinner values
-        Spinner gender = (Spinner) findViewById(R.id.s_gender);
+        Spinner gender = findViewById(R.id.s_gender);
 
         List<String> genders = Arrays.stream(User.Gender.values())
                 .map(Enum::toString)
@@ -82,34 +82,25 @@ public class RegisterUser extends AppCompatActivity {
 
 
         //from https://www.geeksforgeeks.org/how-to-select-an-image-from-gallery-in-android/
-        ib_userPic1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent();
-                i.setType("image/*");
-                i.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE1);
-            }
+        ib_userPic1.setOnClickListener(v -> {
+            Intent i = new Intent();
+            i.setType("image/*");
+            i.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE1);
         });
 
-        ib_userPic2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent();
-                i.setType("image/*");
-                i.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE2);
-            }
+        ib_userPic2.setOnClickListener(v -> {
+            Intent i = new Intent();
+            i.setType("image/*");
+            i.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE2);
         });
 
-        ib_userPic3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent();
-                i.setType("image/*");
-                i.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE3);
-            }
+        ib_userPic3.setOnClickListener(v -> {
+            Intent i = new Intent();
+            i.setType("image/*");
+            i.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE3);
         });
 
         ImageButton ib = findViewById(R.id.ib_usersubmit);
@@ -123,11 +114,13 @@ public class RegisterUser extends AppCompatActivity {
             String userPark = et_park.getText().toString();
 
             // passing input data to UserService
-            // TODO: picture bytearrays must be passed to this function in a List.of(byteArray1, byteArray2, ...)
+            // TODO: DB - picture bytearrays must be passed to this function in a List.of(byteArray1, byteArray2, ...)
+            //  i was not sure if the pictures get saved in the line below or in the function onActivityResult..? - Tobi
             UserService.instance.registerUser(userMail, userPassword, userName, userBirthday, userGender, userBio, userHometown, userLocation, userPark, List.of(pictureArray3));
 
             // UserService.instance.addDog("Doggo", 44, "MALE", true, "SMALL", "YES", "I am a dog.", Set.of("ACTIVE", "DOMINANT"));
 
+            // TODO: DB - id of user as extra for intent? I think it will be needed for addDog - Tobi
             Intent intent = new Intent(getApplicationContext(), RegisterDog.class);
             startActivity(intent);
         });
@@ -137,41 +130,33 @@ public class RegisterUser extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK) {
+        if (resultCode != RESULT_OK) return;
 
-            Uri selectedImageUri = data.getData();
-            if (requestCode == SELECT_PICTURE1) {
-                if (null != selectedImageUri) {
-                    ImageButton userpic1 = findViewById(R.id.ib_userpic1);
-                    userpic1.setImageURI(selectedImageUri);
-                }
-            }
-            else if (requestCode == SELECT_PICTURE2) {
-                if (null != selectedImageUri) {
-                    ImageButton userpic2 = findViewById(R.id.ib_userpic2);
-                    userpic2.setImageURI(selectedImageUri);
-                }
-            }
+        Uri selectedImageUri = data.getData();
+        if (selectedImageUri == null) return;
 
-            else if (requestCode == SELECT_PICTURE3) {
-                if (null != selectedImageUri) {
-                    ImageButton userpic3 = findViewById(R.id.ib_userpic3);
-                    userpic3.setImageURI(selectedImageUri);
-                    try {
-                        //convert bitmap to byte array to save in db, need to be tested
-                        //https://stackoverflow.com/questions/9357668/how-to-store-image-in-sqlite-database#:~:text=Inorder%20to%20store%20images%20to,to%20set%20it%20to%20imageview.
-                        pictureBitmap3 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
-                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                        pictureBitmap3.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                        pictureArray3 = stream.toByteArray();
+        if (requestCode == SELECT_PICTURE1) {
+            ImageButton userpic1 = findViewById(R.id.ib_userpic1);
+            userpic1.setImageURI(selectedImageUri);
+        } else if (requestCode == SELECT_PICTURE2) {
+            ImageButton userpic2 = findViewById(R.id.ib_userpic2);
+            userpic2.setImageURI(selectedImageUri);
+        } else if (requestCode == SELECT_PICTURE3) {
+            ImageButton userpic3 = findViewById(R.id.ib_userpic3);
+            userpic3.setImageURI(selectedImageUri);
+            try {
+                //convert bitmap to byte array to save in db, need to be tested
+                //https://stackoverflow.com/questions/9357668/how-to-store-image-in-sqlite-database#:~:text=Inorder%20to%20store%20images%20to,to%20set%20it%20to%20imageview.
+                pictureBitmap3 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                pictureBitmap3.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                pictureArray3 = stream.toByteArray();
 
-                        //pictureArray3 need to be saved in db as blob, which represents the picture as bytearray
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+                // TODO: DB - pictureArray3 needs to be saved in db as blob, which represents the picture as bytearray
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+
     }
 }
