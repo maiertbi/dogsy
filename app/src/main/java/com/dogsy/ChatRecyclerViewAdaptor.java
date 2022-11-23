32b9896bf1b10184f6383c5077d357a022fa4ccc
zladb <zla_db@naver.com>
2022-11-23 01:56:40 +0000

    resolve warning
    in RecyclerViewAdaptor
    and change the margin top of fragement_user_dog_view.xml

--- app/src/main/java/com/dogsy/ChatRecyclerViewAdaptor.java
+++ app/src/main/java/com/dogsy/ChatRecyclerViewAdaptor.java
@@ -16,7 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class ChatRecyclerViewAdaptor extends RecyclerView.Adapter<ChatRecyclerViewAdaptor.ViewHolder> {
-    private ArrayList<ChatItem> mData = null;
+    private final ArrayList<ChatItem> mData;
 
     public ChatRecyclerViewAdaptor(ArrayList<ChatItem> data) {
         mData = data;
@@ -30,12 +30,10 @@ public class ChatRecyclerViewAdaptor extends RecyclerView.Adapter<ChatRecyclerVi
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         View view = inflater.inflate(R.layout.item_chat, parent, false);
-        ChatRecyclerViewAdaptor.ViewHolder vh = new ChatRecyclerViewAdaptor.ViewHolder(view);
 
-        return vh;
+        return new ViewHolder(view);
     }
 
-    // onBindViewHolder : position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ChatItem item = mData.get(position);
@@ -57,7 +55,7 @@ public class ChatRecyclerViewAdaptor extends RecyclerView.Adapter<ChatRecyclerVi
     }
 
     // 아이템 뷰를 저장하는 뷰홀더 클래스
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
         TextView mainText;
         TextView subText;
